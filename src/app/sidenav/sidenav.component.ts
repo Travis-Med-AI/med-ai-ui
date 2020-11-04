21fd4b5ce680bd9a75444f9bad63bb3f34ff3e09
Travis Clarke <travisjonathanclarke@gmail.com>
2020-11-04 16:23:40 -0500

    Version 0.1

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -21,7 +21,7 @@ export class SidenavComponent implements OnInit {
   notifications: NotificationMessage[] = []
   sidenavItems: sideNavItem[] = [
     {icon: 'insert_drive_file', routerLink: 'new-job', displayValue: 'Studies'},
-    {icon: 'model_training', routerLink: 'evaluate-study', displayValue: 'Evaluate'},
+    // {icon: 'model_training', routerLink: 'evaluate-study', displayValue: 'Evaluate'},
     {icon: 'add_to_queue', routerLink: 'jobs', displayValue: 'Models'},
     {icon: 'leaderboard', routerLink: 'evals', displayValue: 'Evals'},
     {icon: 'memory', routerLink: 'monitor', displayValue: 'Monitor'},
